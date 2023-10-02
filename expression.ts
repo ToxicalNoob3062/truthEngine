c0942f01a2f1a3feb2abf791bdbf392d6d5b8699
Rahat <rahat3062@gmail.com>
2023-10-02 08:17:43 -0400

    brackets can be synthesized now by the help of analyzer

--- expression.ts
+++ expression.ts
@@ -4,37 +4,28 @@ import PriorityQueue from "./priorityQueue";
 export default class Expression {
   //will help us to evaluate expression for certain operation
   exp: string;
-  exp_copy: string;
   variableHash: object;
-  inputSets: string[];
+  input: string;
   operatorsQueue: PriorityQueue<string>;
-  solveForCases: string[] = [];
 
-  constructor(exp: string) {
-    this.exp = this.exp_copy = exp;
-    this.extractInput();
+  //constructor function to solve a expression without bracket for single input set
+  constructor(exp: string, hashes: object, input: string) {
+    this.exp = exp;
+    this.variableHash = hashes;
+    this.input = input;
     this.arrangeOperators();
-    this.solveExpression();
+    this.solve();
   }
-  private solveExpression() {
+
+  private solve() {
     // console.log("Expression to solve: " + this.exp); //stating problem expression
-    const priorityMultiple = this.operatorsQueue.size();
-    for (let i = 0; i < this.inputSets.length; i++) {
-      let repeatations: number = priorityMultiple;
-      // console.log(`For case${i}:${this.inputSets[i]}`); //label for cases
-      while (repeatations-- > 0) {
-        const operator = this.operatorsQueue.dequeue();
-        this.monsterOperator(i, operator as string);
-        this.operatorsQueue.enqueue(
-          operator as string,
-          priorityMultiple + i + 1
-        );
-      }
-      this.solveForCases.push(this.exp); //each is a value of the rows for a specific column!
-      this.exp = this.exp_copy;
+    // console.log(`For case${i}:${this.inputSets[i]}`); //label for cases
+    while (this.operatorsQueue.size() > 0) {
+      const operator = this.operatorsQueue.dequeue();
+      this.monsterOperator(operator as string);
     }
   }
-  private monsterOperator(setNo: number, operator: string) {
+  private monsterOperator(operator: string) {
     //let set to constant
     for (let ind = 0; ind < this.exp.length; ind++) {
       let char = this.exp[ind];
@@ -43,8 +34,8 @@ export default class Expression {
         const right = this.exp[ind + 1] ? this.exp[ind + 1] : "F";
         const result = this.calculate(
           operator,
-          this.searchValue(left, setNo),
-          this.searchValue(right, setNo)
+          this.searchValue(left),
+          this.searchValue(right)
         );
         this.exp = this.stringMutation(
           operator == "~" ? ind : ind - 1,
@@ -79,27 +70,6 @@ export default class Expression {
       this.operatorsQueue.enqueue(operator, scores[operator])
     );
   }
-  private extractInput() {
-    this.variableHash = {};
-    this.inputSets = [];
-    let serial = 0;
-    for (let char of this.exp) {
-      if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
-        if (this.variableHash[char] === undefined) {
-          this.variableHash[char] = serial++;
-        }
-      }
-    }
-    const findCombinations = (pathTrack: string = "", loopNo: number = 1) => {
-      if (loopNo > serial) {
-        this.inputSets.push(pathTrack);
-        return;
-      }
-      findCombinations(pathTrack + "T", loopNo + 1);
-      findCombinations(pathTrack + "F", loopNo + 1);
-    };
-    findCombinations();
-  }
   private stringMutation(start: number, end: number, include: string) {
     let firstPart = this.exp.slice(0, start < 0 ? 0 : start);
     const lastPart = this.exp.slice(end + 1);
@@ -108,10 +78,9 @@ export default class Expression {
   private booleanToString(bool: boolean) {
     return bool ? "T" : "F";
   }
-  private searchValue(variable: string, setNo: number) {
+  private searchValue(variable: string) {
     if (variable == "T" || variable == "F") return variable;
-    const input = this.inputSets[setNo];
-    const variableValue = input[this.variableHash[variable]];
+    const variableValue = this.input[this.variableHash[variable]];
     return variableValue;
   }
   private calculate(operator: string, left: string, right: string) {
@@ -137,6 +106,3 @@ export default class Expression {
     }
   }
 }
-
-const expression = new Expression("p&q&r|p");
-// console.log(expression.solveForCases);
--- parenthesis.ts
+++ parenthesis.ts
@@ -1,5 +1,75 @@
 import Expression from "./expression";
 
 export default class Analayzer {
-  solutionMap: Map<string, string[]>;
+  solutionMap: object = {};
+  variableHash: object = {};
+  inputSets: string[] = [];
+  exp: string;
+
+  constructor(exp: string) {
+    this.exp = exp;
+    this.extractInput();
+    for (let inputIndex = 0; inputIndex < this.inputSets.length; inputIndex++) {
+      this.inspectBrackets(this.exp, inputIndex);
+    }
+  }
+
+  private inspectBrackets(exp: string, inputIndex: number) {
+    const stack: number[] = [];
+    const copy = exp;
+    for (let ind = 0; ind < exp.length; ind++) {
+      const char = exp[ind];
+      if (char === "(") {
+        stack.push(ind);
+      } else if (char == ")") {
+        const start = stack.pop() as number;
+        const content = exp.slice(start + 1, ind);
+        const result = this.inspectBrackets(content, inputIndex);
+        exp = this.resolveBracket(start, ind, result, exp);
+        ind -= ind - start;
+      }
+    }
+    const ans = new Expression(
+      exp,
+      this.variableHash,
+      this.inputSets[inputIndex]
+    ).exp;
+    const column = this.solutionMap[copy];
+    column ? column.push(ans) : (this.solutionMap[copy] = [ans]);
+    return ans;
+  }
+
+  private resolveBracket(
+    start: number,
+    end: number,
+    include: string,
+    exp: string
+  ) {
+    let firstPart = exp.slice(0, start);
+    const lastPart = exp.slice(end + 1);
+    return firstPart + include + lastPart;
+  }
+
+  private extractInput() {
+    let serial = 0;
+    for (let char of this.exp) {
+      if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+        if (this.variableHash[char] === undefined) {
+          this.variableHash[char] = serial++;
+        }
+      }
+    }
+    const findCombinations = (pathTrack: string = "", loopNo: number = 1) => {
+      if (loopNo > serial) {
+        this.inputSets.push(pathTrack);
+        return;
+      }
+      findCombinations(pathTrack + "T", loopNo + 1);
+      findCombinations(pathTrack + "F", loopNo + 1);
+    };
+    findCombinations();
+  }
 }
+
+const expression = new Analayzer("(p|q)&(~q|r&p)|~r");
+console.log(expression.solutionMap);
